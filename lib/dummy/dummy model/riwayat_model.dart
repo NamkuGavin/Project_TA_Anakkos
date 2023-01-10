c73fb8736883a47f5e8374572234c4343cf42fb6
GAVIN <m.gavin.aga10@gmail.com>
2023-01-10 10:58:48 +0700

    feat: [GA] History Page

---
+++ lib/dummy/dummy model/riwayat_model.dart
@@ -0,0 +1,46 @@
+class RiwayatDummyModel {
+  String picture_riwayat;
+  String jenis_kostRiwayat;
+  String nama_riwayat;
+  String lokasi_riwayat;
+  String tanggal_riwayat;
+  String waktu_riwayat;
+  String status_riwayat;
+
+  RiwayatDummyModel(
+      this.picture_riwayat,
+      this.jenis_kostRiwayat,
+      this.nama_riwayat,
+      this.lokasi_riwayat,
+      this.tanggal_riwayat,
+      this.waktu_riwayat,
+      this.status_riwayat);
+
+  String get getPicture {
+    return picture_riwayat;
+  }
+
+  String get getJenis {
+    return jenis_kostRiwayat;
+  }
+
+  String get getNama {
+    return nama_riwayat;
+  }
+
+  String get getLokasi {
+    return lokasi_riwayat;
+  }
+
+  String get getTanggal {
+    return tanggal_riwayat;
+  }
+
+  String get getWaktu {
+    return waktu_riwayat;
+  }
+
+  String get getStatus {
+    return status_riwayat;
+  }
+}
